9f923b385cf83b8fc4706ceaadacd875dfba834d
16IMA <imonfort001@gmail.com>
2025-03-17 22:12:30 +0100

    ep post and put

--- src/index.js
+++ src/index.js
@@ -1,164 +1,143 @@
-//Importar bibliotecas
-
+require('dotenv').config();
 const express = require('express');
 const cors  = require('cors');
-const mysql = require('musql2/promise');
-require('dotenv').confg();
-
-//Inicializar y configurar Express
+const mysql = require('mysql2/promise');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-//Arrancar servidor
-const port = process.env['PORT'];
- app.listen(port, {} => {
-    console.log(`Server funcionando en el puerto ${port}`)
- });
 
+console.log('Puerto:', process.env.PORT);
 
-// Funciones
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`Server funcionando en el puerto ${port}`);
+});
 
 async function getConnection() {
-
     const connectionData = {
-        host: process.env.['MYSQL_HOST'],
-        port: process.env.['MYSQL_PORT'],
-        user: process.env.['MYSQL_USER'],
-        password: process.env.['MYSQL_PASSWORD'],
-        database: process.env.['MYSQL_DATABASE']
-    }
+        host: process.env.MYSQL_HOST,
+        port: Number(process.env.MYSQL_PORT),
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE
+    };
 
     const connection = await mysql.createConnection(connectionData);
-
     await connection.connect();
-
     return connection;
+}
+
+// EndPoints
+app.get('/api/weddingmusic/wedding', async (req, res) => {
+    
+    const search = req.query.search ?
+        `%${req.query.search}%` :
+        '%';
+   
+    let sql = 'SELECT * FROM wedding_music.wedding WHERE 1=1'; 
+
+    const params = [];
+
+    if (req.query.date) {
+        sql += ' AND date = ?';
+        params.push(req.query.date);
     }
 
-//EndPoints
+    if (req.query.spouse_one_name) {
+        sql += ' AND spouse_one_name LIKE ?';
+        params.push(search);
+    }
 
-app.get('/api/weddingmusic/wedding', async (req, res) => {
-    //SELECT
-    
-    // Preparar la sentencia SQL
-
-    const search = !req.query.search ? 
-        '%' :
-        '%' + req.query.search + '%';
-
-    //Conexión a la base de datos MySQL
+    if (req.query.spouse_two_name) {
+        sql += ' AND spouse_two_name LIKE ?';
+        params.push(search);
+    }
 
     const conn = await getConnection();
 
-    //Lanzamos sentencia SQL
+    try {
+        const [result] = await conn.query(sql, params);
 
-    const [result] = conn.query(`
-        SELECT * 
-        FROM wedding_music.wedding;`);
+        await conn.end();
+        res.json(result);
 
-    //WHERE spouse_one_name LIKE ?`, [search]
+    } catch (error) {
 
-    //Cerramos la conexión
-    await conn.end();
-
-    //Respondo
-
-    res.json(result)
-
+        await conn.end();
+        res.status(500).json({ success: false, error:'Boda no encontrada' });
+    }
 });
 
+
 app.post('/api/weddingmusic/wedding', async (req, res) => {
-    //INSERT
-
-    // Preparar la sentencia SQL
-
-    if(req.body.date || req.body.spouse_one_name || req.body.spouse_two_name || req.body.localitation || === ' ') {
+    
+    if (!req.body.date || !req.body.spouse_one_name || !req.body.spouse_two_name || !req.body.localitation) {
         return res.status(400).json({ 
             success: false,
-            error: 'Faltan datos obligatorios' });
-    });
+            error: 'Faltan datos obligatorios' 
+        });
+    }
 
-    //Conexión a la base de datos MySQL
-
-    const conn = await getConnection(`
+    const conn = await getConnection();
+    
+    const [result] = await conn.execute(`
         INSERT INTO wedding_music.wedding
             (date, spouse_one_name, spouse_two_name, localitation)
-        VALUES ('?', '?', '?', '?')`,
+        VALUES (?, ?, ?, ?)`,
         [req.body.date, req.body.spouse_one_name, req.body.spouse_two_name, req.body.localitation]);
-
-    //Lanzamos el resultado
-
-    const [result] = await conn.execute();
-
-    //Cerramos conexión
 
     await conn.end();
 
-    //Respondo
-
     res.json({
-        success: true,
+        success: true,  
         wedding_id: result.insertId,
-        message: 'Wedding registrada correctamente'
-        obj: {
+        message: 'Boda registrada correctamente',
+        weddingObj: {
             ...req.body,
             wedding_id: result.insertId,
         }
     });
+});
 
 app.put('/api/weddingmusic/wedding', async (req, res) => {
-    //UPDATE
+    
+    const wedding = req.body;
 
-    // Preparar la sentencia SQL
-
-    //Conexión a la base de datos MySQL
+    if( !wedding.wedding_id || isNaN(parseInt(wedding.wedding_id)) ) {
+    return res.status(400).json({
+      success: false,
+      error: 'El Id no es un número'
+    });
+  }
 
     const conn = await getConnection();
 
-    //Lanzamos el resultado
+    const [result] = await conn.execute(
+    `
+    UPDATE wedding_music.wedding
+      SET date = ?, spouse_one_name = ?, spouse_two_name = ?, localitation = ?
+      WHERE wedding_id = ?`,
+    [ wedding.date, wedding.spouse_one_name, wedding.spouse_two_name, wedding.localitation ] 
+    
+  );
 
-    //Respondo
+  // Cierro la conexión
 
-    res.send('{},{}')
+  await conn.end();
 
+  // Respodo
+
+  res.json({
+    success: true,
+    updatedWedding: {
+      ...req.body
+    }
+});
 });
 
 app.delete('/api/weddingmusic/wedding', async (req, res) => {
-    //DELETE
-
-    // Preparar la sentencia SQL
-
-    //Conexión a la base de datos MySQL
-
-    const conn = await getConnection();
-
-    //Lanzamos el resultado
-
-    //Respondo
-
-    res.send('{},{}')
-
-});
-
-
-//Conexión a la base de datos MySQL
-
-app.get('/api/weddingmusic/wedding', async (req, res) => {
-    //SELECT
-
-    // Preparar la sentencia SQL
-
-    //Conexión a la base de datos MySQL
-
-    const conn = await getConnection();
-
-    //Lanzamos el resultado
-
-    //Respondo
-
-    res.send('{},{}')
-
+    res.send({ message: 'Endpoint sin usar' });
 });
